547d01ace8991d3ec196e97666399d4e0be23040
vamsi113 <krishultimate5@gmail.com>
2023-06-06 19:08:20 -0400

    change

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,7 @@ resource "aws_launch_template" "launch-template" {
 }
 
 resource "aws_autoscaling_group" "asg" {
+  name = "${var.env}-${var.name}-asg"
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
