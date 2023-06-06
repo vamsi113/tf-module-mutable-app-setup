6e0c3833db71bf6ad33f4ae7d9f585fe9e97c87e
vamsi113 <krishultimate5@gmail.com>
2023-06-06 18:57:55 -0400

    change

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_launch_template" "launch-template" {
+  name_prefix   =  "${var.env}-${var.name}-lt"
+  image_id      = data.aws_ami.centos-8-ami.image_id
+  instance_type = "c5.large"
+}
+
+rresource "aws_autoscaling_group" "asg" {
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "subnets" {}
+variable "instance_type" {}
+variable "name" {}
+variable "max_size" {}
+variable "min_size" {}
+
+
+
