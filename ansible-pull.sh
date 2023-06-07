d5cd0c70b24927719d760943f72f7cd30e8a871c
vamsi113 <krishultimate5@gmail.com>
2023-06-06 21:51:32 -0400

    change

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -4,4 +4,5 @@ LOG=/opt/user-data-$$.log
 yum install python39-devel -y &>>$LOG
 pip3.9 install ansible botocore boto3 python-jenkins &>>$LOG
 
-ansible-pull -i localhost, -U https://github.com/vamsi113/roboshop-ansible.git roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV}
+ansible-pull -i localhost, -U https://github.com/vamsi113/roboshop-ansible.git roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV} &>>$LOG
+
---
+++ iam.tf
@@ -0,0 +1,52 @@
+resource "aws_iam_policy" "policy" {
+  name        = "${var.env}-${var.name}-policy"
+  path        = "/"
+  description = "${var.env}-${var.name}-elasticache"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "secretsmanager:GetSecretValue",
+          "ssm:GetParameter",
+          "secretsmanager:ListSecrets"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.name}-policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "policy-to-role-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
