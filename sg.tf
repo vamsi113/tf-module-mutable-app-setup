8435ab4ca359ecbaec3c95c05ee3450da0e8a7d3
vamsi113 <krishultimate5@gmail.com>
2023-06-06 22:30:52 -0400

    change

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "sg" {
+  name        = "${var.env}-${var.name}-ec2.sg"
+  description = "${var.env}-${var.name}-ec2.sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.BASTION_NODE]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-${var.name}-ec2.sg"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -4,6 +4,8 @@ variable "instance_type" {}
 variable "name" {}
 variable "max_size" {}
 variable "min_size" {}
+variable "vpc_id" {}
+variable "BASTION_NODE" {}
 
 
 
