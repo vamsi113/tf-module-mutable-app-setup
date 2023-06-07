1451bae256eb65da50dd3883976aafa98cf184ef
vamsi113 <krishultimate5@gmail.com>
2023-06-06 23:08:29 -0400

    change

--- asg.tf
+++ asg.tf
@@ -13,20 +13,20 @@ resource "aws_launch_template" "launch-template" {
   }))
 }
 
-#resource "aws_autoscaling_group" "asg" {
-#  name                = "${var.env}-${var.name}-asg"
-#  max_size            = var.max_size
-#  min_size            = var.min_size
-#  vpc_zone_identifier = var.subnets
-#
-#  launch_template {
-#    id      = aws_launch_template.launch-template.id
-#    version = "$Latest"
-#  }
-#  tag {
-#    key                 = "Name"
-#    propagate_at_launch = true
-#    value               = "${var.env}-${var.name}"
-#  }
-#}
-#
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.env}-${var.name}-asg"
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnets
+
+  launch_template {
+    id      = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    propagate_at_launch = true
+    value               = "${var.env}-${var.name}"
+  }
+}
+
