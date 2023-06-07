6e7e1b5864fcc0678bceee170f2686d464fc0e39
vamsi113 <krishultimate5@gmail.com>
2023-06-06 21:16:46 -0400

    change

--- asg.tf
+++ asg.tf
@@ -1,27 +1,27 @@
-#resource "aws_launch_template" "launch-template" {
-#  name          = "${var.env}-${var.name}-lt"
-#  image_id      = data.aws_ami.centos-8-ami.image_id
-#  instance_type = var.instance_type
-#  user_data = base64encode(templatefile("${path.module}/ansible-pull.sh", {
-#    COMPONENT = var.name
-#    ENV       = var.env
-#  }))
-#}
-#
-#resource "aws_autoscaling_group" "asg" {
-#  name                = "${var.env}-${var.name}-asg"
-#  max_size            = var.max_size
-#  min_size            = var.min_size
-#  vpc_zone_identifier = var.subnets
-#
-#  launch_template {
-#    id      = aws_launch_template.launch-template.id
-#    version = "$Latest"
-#  }
-#  tag {
-#    key                 = "Name"
-#    propagate_at_launch = true
-#    value               = "${var.env}-${var.name}"
-#  }
-#}
-#
+resource "aws_launch_template" "launch-template" {
+  name          = "${var.env}-${var.name}-lt"
+  image_id      = data.aws_ami.centos-8-ami.image_id
+  instance_type = var.instance_type
+  user_data = base64encode(templatefile("${path.module}/ansible-pull.sh", {
+    COMPONENT = var.name
+    ENV       = var.env
+  }))
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.env}-${var.name}-asg"
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnets
+
+  launch_template {
+    id      = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    propagate_at_launch = true
+    value               = "${var.env}-${var.name}"
+  }
+}
+
