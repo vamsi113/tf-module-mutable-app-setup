28f5a979875550482ed86014707a84a9131a06d1
vamsi113 <krishultimate5@gmail.com>
2023-06-06 22:05:21 -0400

    change

--- asg.tf
+++ asg.tf
@@ -1,13 +1,17 @@
-#resource "aws_launch_template" "launch-template" {
-#  name          = "${var.env}-${var.name}-lt"
-#  image_id      = data.aws_ami.centos-8-ami.image_id
-#  instance_type = var.instance_type
-#  user_data = base64encode(templatefile("${path.module}/ansible-pull.sh", {
-#    COMPONENT = var.name
-#    ENV       = var.env
-#  }))
-#}
-#
+resource "aws_launch_template" "launch-template" {
+  name          = "${var.env}-${var.name}-lt"
+  image_id      = data.aws_ami.centos-8-ami.image_id
+  instance_type = var.instance_type
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.instance_profile.name
+  }
+  user_data = base64encode(templatefile("${path.module}/ansible-pull.sh", {
+    COMPONENT = var.name
+    ENV       = var.env
+  }))
+}
+
 #resource "aws_autoscaling_group" "asg" {
 #  name                = "${var.env}-${var.name}-asg"
 #  max_size            = var.max_size
