a4a7c5876492a9352e6ee7db818822731435bc5f
vamsi113 <krishultimate5@gmail.com>
2023-06-08 16:53:18 -0400

    change

---
+++ target--groups.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_target_group" "main" {
+  name     = "${var.env}-${var.name}-tg"
+  port     = var.app_port_no
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    enabled = true
+  }
+  healthy_threshold   = 2
+  unhealthy_threshold = 2
+  timeout             = 30
+  interval            = 5
+  path                = "/health"
+}
+
