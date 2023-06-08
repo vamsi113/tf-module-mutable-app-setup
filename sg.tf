6f637ebb00e156f117d1644aa8a8d5365fa64bcf
vamsi113 <krishultimate5@gmail.com>
2023-06-08 12:09:58 -0400

    change

--- sg.tf
+++ sg.tf
@@ -11,6 +11,22 @@ resource "aws_security_group" "sg" {
     cidr_blocks = [var.BASTION_NODE]
   }
 
+  ingress {
+    description = "APP"
+    from_port   = var.app_port_no
+    to_port     = var.app_port_no
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  ingress {
+    description = "PROMETHEUS"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = [var.PROMETHEUS_NODE]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
--- vars.tf
+++ vars.tf
@@ -6,6 +6,10 @@ variable "max_size" {}
 variable "min_size" {}
 variable "vpc_id" {}
 variable "BASTION_NODE" {}
+variable "app_port_no" {}
+variable "PROMETHEUS_NODE" {}
+variable "vpc_cidr" {}
+
 
 
 
