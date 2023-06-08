f27e27d4982f896c1af4a29176994c45fb953576
vamsi113 <krishultimate5@gmail.com>
2023-06-08 18:09:26 -0400

    change

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "private" {
   zone_id = var.private_zone_id
-  name    = "${var.name}-${var.env}.roboshop.internal"
+  name    = "${var.name}-${var.env}"
   type    = "CNAME"
   ttl     = 300
   records = [var.alb["private"].lb_dns_name]
