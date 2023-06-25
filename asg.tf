e49810e5ed3cc118e991957b0286426df90264e4
vamsi113 <krishultimate5@gmail.com>
2023-06-24 20:49:47 -0400

    change

--- asg.tf
+++ asg.tf
@@ -35,3 +35,14 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
+resource "aws_autoscaling_policy" "cpu-tracking-policy" {
+  name        = "whenCPULoadIncrease"
+  policy_type = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0
+  }
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
